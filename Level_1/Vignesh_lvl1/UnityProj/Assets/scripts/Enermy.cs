b942a0268a1a6ac26458c365184ab0022f6dbc55
Vignesh R <89893905+MilesMora@users.noreply.github.com>
2025-10-30 22:35:07 +0530

    Contributions to lvl 1 by Vignesh
    Bugs to be expected.

---
+++ Level_1/Vignesh_lvl1/UnityProj/Assets/scripts/Enermy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Enermy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
